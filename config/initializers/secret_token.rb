04140d86471969066907f098353e79a87a2a948d
billy <ruozhoufan@gmail.com>
2012-06-03 21:14:03 -0400

    Init hktest repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hktest::Application.config.secret_token = 'd8c10d25f8f2a3bdf3e68c5b5d5c7faacf14c5098fff7197ca899e35fbf5d5c390e4a6e348eb2ce7b10d736b8a7defe4fa46a0b27591a1153a57183eeeebbd07'
